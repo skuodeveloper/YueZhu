a42e40be1549f4ba2ce62b7abe4f22dc2587def9
吴文奇 <51804474@qq.com>
2017-05-04 15:22:15 +0800

    test

--- app/src/main/java/com/skuo/yuezhu/MainActivity.java
+++ app/src/main/java/com/skuo/yuezhu/MainActivity.java
@@ -3,11 +3,15 @@ package com.skuo.yuezhu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.logging.Logger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Logger.getGlobal();
     }
 }
