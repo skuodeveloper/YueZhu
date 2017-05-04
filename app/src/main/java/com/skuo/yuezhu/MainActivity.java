dc1358d9a477afac5b7d8f15d54a207e870d894c
吴文奇 <51804474@qq.com>
2017-05-04 15:25:27 +0800

    test

--- app/src/main/java/com/skuo/yuezhu/MainActivity.java
+++ app/src/main/java/com/skuo/yuezhu/MainActivity.java
@@ -1,9 +1,7 @@
 package com.skuo.yuezhu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.util.logging.Logger;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Logger.getGlobal();
     }
 }
